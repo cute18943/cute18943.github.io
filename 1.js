9b15940b8c8232692f823b29e6da49e994ad0852
cute18943 <33677825+cute18943@users.noreply.github.com>
2020-05-06 15:44:27 +0800

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,46 @@
+function In(){
+document.my.src="picture/111.jpg"
+}
+function Out(){
+document.my.src="picture/11.jpg"
+}
+function In1(){
+document.my1.src="picture/222.jpg"
+}
+function Out1(){
+document.my1.src="picture/22.jpg"
+}
+function In2(){
+document.my2.src="picture/333.jpg"
+}
+function Out2(){
+document.my2.src="picture/33.jpg"
+}
+
+function In3(){
+document.my3.src="picture/444.jpg"
+}
+function Out3(){
+document.my3.src="picture/44.jpg"
+}
+
+function In4(){
+document.my4.src="picture/555.jpg"
+}
+function Out4(){
+document.my4.src="picture/55.jpg"
+}
+function In5(){
+document.my5.src="picture/666.jpg"
+}
+function Out5(){
+document.my5.src="picture/66.jpg"
+}
+function In6(){
+document.my6.src="picture/chl.jpg"
+document.my6.border="1"
+}
+function Out6(){
+document.my6.src="picture/chl.jpg"
+document.my6.border="0"
+}
